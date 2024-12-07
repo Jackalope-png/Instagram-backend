a437b0462e632ac4acb0e81df492164e12dec0b2
24HP6314 24HP6314 <24HP6314@admins-iMac.local>
2024-12-07 11:07:33 +0800

    Cors

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
 const app = express();
 const UserRoutes = require('./routes/userRoutes');
 const postRoutes = require('./routes/postRoutes');
 const likeRoutes = require('./routes/likeRoutes')
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
