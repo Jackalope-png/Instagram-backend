299ac3f6dc80d3c8b7267770f6b6ce3c9902de41
24HP6314 24HP6314 <24HP6314@admins-iMac.local>
2024-12-19 17:42:41 +0800

    tiny add

--- authMiddlewares.js
+++ authMiddlewares.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const authMiddleware = () =>{
+    const token = req.headers['authorization']?.split(' ')[1];
      if (!token) {
         return resizeBy.status(403).send('Token is required for authentication')
     }
