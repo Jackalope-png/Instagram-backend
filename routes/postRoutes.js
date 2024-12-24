af051f8bb76c5f7ab24f62341a3fdd9fc5bf1604
24HP6314 24HP6314 <24HP6314@admins-iMac.local>
2024-12-24 17:59:59 +0800

    final fix hopefully

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -5,10 +5,10 @@ const router = express.Router();
 
 
 // Create a new post
-router.post('/posts', postController.createPost);
+router.post( postController.createPost);
 
 // Get all posts with likes populated
-router.get('/posts', authMiddleware, postController.getAllPosts);
+router.get( authMiddleware, postController.getAllPosts);
 
 // Get posts by user with likes populated
 router.get('/user/:userId', postController.getPostsByUser);
