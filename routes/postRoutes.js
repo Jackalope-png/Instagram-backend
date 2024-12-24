44d1cfa8da32de872c33b60a9d0af9298f94cf9f
24HP6314 24HP6314 <24HP6314@admins-iMac.local>
2024-12-24 17:42:39 +0800

    trying to fix test

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -5,10 +5,10 @@ const router = express.Router();
 
 
 // Create a new post
-router.post('/', postController.createPost);
+router.post('/posts', postController.createPost);
 
 // Get all posts with likes populated
-router.get('/', authMiddleware, postController.getAllPosts);
+router.get('/posts', authMiddleware, postController.getAllPosts);
 
 // Get posts by user with likes populated
 router.get('/user/:userId', postController.getPostsByUser);
