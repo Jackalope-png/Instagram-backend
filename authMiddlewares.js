fd195745696fbc5538a2fa0cef627fd4d8c13ae2
24HP6314 24HP6314 <24HP6314@admins-iMac.local>
2024-12-26 16:15:53 +0800

    final fix

--- authMiddlewares.js
+++ authMiddlewares.js
@@ -2,13 +2,13 @@ const jwt = require('jsonwebtoken');
 const authMiddleware = (req, res, next) =>{
     const token = req.headers['authorization']?.split(' ')[1];
      if (!token) {
-        return resizeBy.status(403).send('Token is required for authentication')
+        return res.status(403).send('Token is required for authentication')
     }
-    jwt.verify(token, process.env.JWT_SECRET, (err, decorded) =>{
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) =>{
         if (err){
             return res.status(403).send('invalid token')
         }
-        req.userID = decorded.id
+        req.userID = decoded.id
         next();
     })
 }
